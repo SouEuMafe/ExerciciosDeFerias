cef93ee2bf8fb6b1f4ad33ff3f88a8397ef57ba2
Maria Fernanda Galdino <135236915+SouEuMafe@users.noreply.github.com>
2025-02-03 13:04:55 -0300

    Add files via upload

---
+++ Exercícios cap 1.c
@@ -0,0 +1,23 @@
+//Exercícios capítulo 1 do livro do André Backers
+//Exercício 1, Cap 1
+
+#include <stdio.h>
+int main(){
+    printf("Início do programa.\n Fim.\n")
+    return 0;
+
+}
+
+// Exercício 2, cap 1
+#include <stdio.h>
+
+int main() {
+    int num;  // Adicionado o ponto e vírgula
+
+    printf("Insira um número inteiro:\n");
+    scanf("%d", &num);
+
+    return 0;
+}
+
+//Exercício 3, cap 1
